616824ba81ca1a911135078093d2b9564c0729fd
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-05 16:13:54 -0430

    vista de tarea especifica

--- app/controllers/task_types_controller.rb
+++ app/controllers/task_types_controller.rb
@@ -14,6 +14,17 @@ class TaskTypesController < ApplicationController
 		end
 	end
 
+	def show
+		aux = Task.exists?((params[:id].to_i))
+	 	if aux == true
+	 		task = Task.find((params[:id].to_i))
+	 		render json: task.to_json
+	 	else
+	 		render json: {"Error" => "Error 404 La Tarea no existe"}
+	 	end
+			
+	end
+
 	def permit
 		params.permit(:title, :finish_date, :status, :task_type)
 	end	
