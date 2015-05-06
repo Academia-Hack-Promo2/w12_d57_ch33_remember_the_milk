dd4a6e54a3e700a0bddc19743b5b2bfddc6a6645
John Galindez <galindezj2@gmail.com>
2015-05-06 16:58:11 -0430

    Validando en tipo de tareas

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
 	validates :title, presence: true
 	validates :status, length: {maximum: 10}
-	validates :task_type,length: {maximum: 15}
+	validates :task_type,length: {maximum: 15}, inclusion: { in: %w(llamada citas diligencias recordatorio),
+    message: "%{value} El tipo de tarea no esta permitido" }
   belongs_to :category
 end
