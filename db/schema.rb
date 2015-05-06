abd9a1205018bde22e902942bd1b06eb8538f6aa
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-06 10:33:27 -0430

    agregado category mig,mod

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505160245) do
+ActiveRecord::Schema.define(version: 20150506143645) do
+
+  create_table "categories", force: true do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: true do |t|
     t.string   "title",                  null: false
