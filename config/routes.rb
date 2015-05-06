1de9316bb681e07ae2d30e283bcaff87f6a761b8
maria <mariiromero17@gmail.com>
2015-05-06 12:28:00 -0430

    index y delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :tasks
-  resources :categories
+  resources :tasks, except: ["new","edit"]
+  resources :categories, except: ["new","edit"]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
